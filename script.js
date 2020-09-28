5fc94d87beada7d9fb9c7c26015cd310e6c95b77
Tom Bousquet <tom.bousquet@alumni.unc.edu>
2020-09-28 09:22:51 -0400

    date object

--- script.js
+++ script.js
@@ -17,7 +17,28 @@ function validateForm (event) {
     removeErrorMessage()
     formIsValid = true
 
- 
+function validateCardNumber(number) {
+        var regex = new RegExp("^[0-9]{16}$");
+        if (!regex.test(number))
+            return false;
+    
+        return luhnCheck(number);
+    }
+    
+function luhnCheck(val) {
+        var sum = 0;
+        for (var i = 0; i < val.length; i++) {
+            var intVal = parseInt(val.substr(i, 1));
+            if (i % 2 == 0) {
+                intVal *= 2;
+                if (intVal > 9) {
+                    intVal = 1 + (intVal % 10);
+                }
+            }
+            sum += intVal;
+        }
+        return (sum % 10) == 0;
+    }
 
     if (formIsValid) {
         let total = document.querySelector('#total')
@@ -40,6 +61,10 @@ function validateForm (event) {
         })
     }
    
+//#5 id start date, id # of days staying, 
+//id which days are during the week/weekend
+//multiply 5|7 depending and sum
+
 }
 
 function removeErrorMessage () {
